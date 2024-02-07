908798d3e4f27fb050969655d987e6f3f62faf97
Sara <sado0001@stud.kea.dk>
2024-02-07 13:19:07 +0100

    alt

--- kategori.js
+++ kategori.js
@@ -7,12 +7,13 @@ function showCategories(cats) {
 }
 
 function showCategory(cat) {
-  const template = document.querySelector("template").content;
+  const template = document.querySelector(".template1").content;
 
   const clone = template.cloneNode(true);
 
   clone.querySelector("a").textContent = cat.category;
   clone.querySelector("a").href = `produktliste.html?category=${cat.category}`;
+  clone.querySelector(".list").classList.add("border");
 
   document.querySelector(".letterGroup ol").appendChild(clone);
 }
